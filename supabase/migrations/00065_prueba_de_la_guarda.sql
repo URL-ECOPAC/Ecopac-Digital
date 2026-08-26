6833014516adb34a47d33baeb5ce0ca25ec14538
Wilder Menchú <menchuwilder@gmail.com>
2026-08-26 16:22:42 -0600

    test(infra): duplicar el prefijo 00065 para ver la guarda en rojo

---
+++ supabase/migrations/00065_prueba_de_la_guarda.sql
@@ -0,0 +1,4 @@
+-- Archivo temporal para comprobar la guarda de numeracion (issue #411).
+-- Duplica a proposito el prefijo 00065, que ya usa 00065_fn_existencias_disponibles.sql.
+-- El commit siguiente lo borra: no debe llegar a develop.
+SELECT 1;
