c865cc7540c5c1de08b2f70f1b7c490bb2ca760b
AnderNoleon <79151721+AnderNoleon@users.noreply.github.com>
2026-08-26 21:48:12 -0600

    Revert "Update 0068"
    This reverts commit 35c26b9c1adc4cc93b9ff8415006a743f1439695.

--- supabase/migrations/00068_buscar_pacientes.sql
+++ supabase/migrations/00068_buscar_pacientes.sql
@@ -89,8 +89,6 @@
 -- columna de salida homonima de tres columnas de entrada es el tipo de colision que conviene
 -- evitar por diseno.
 
-SELECT extensions.word_similarity('', '');
-
 CREATE OR REPLACE FUNCTION fn_buscar_pacientes(
   p_termino TEXT DEFAULT NULL,
   p_comunidad_id UUID DEFAULT NULL,
